e701d457fd42c95c4d5b1d0c7e53ee044b533ed9
糖小米 <tangxiaomiemail@gmail.com>
2019-07-27 11:20:07 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+npm install -g vuepress
+# npm install vuepress-theme-travis
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd .vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'tangxiaomi.top' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://${access_token}@github.com/im/vuepress-theme-postline.git gh-pages
+
+cd -
