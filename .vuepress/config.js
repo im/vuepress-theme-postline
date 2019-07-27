4846d6a65f39397651eb6cbd9d5ed4adcf428ea9
糖小米 <tangxiaomiemail@gmail.com>
2019-07-27 11:19:13 +0800

    new update

---
+++ .vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    port: 9090,
+    title: 'vuepress-theme-travis',
+    description: 'vuepress-theme-travis',
+    // theme: 'travis',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        authorInfo: {
+            headerUrl: 'https://avatars3.githubusercontent.com/u/12029165?s=460&v=4',
+            name: '糖小米',
+            github: 'https://github.com/im'
+        },
+        lastUpdated: true,
+        search: {},
+        category: {}
+        // nav: [
+        //     { text: 'Home', link: '/' },
+        //     { text: 'Guide', link: '/guide/' },
+        //     { text: 'External', link: 'https://google.com' },
+        //     {
+        //         text: 'Languages',
+        //         items: [
+        //             { text: 'Chinese', link: '/language/chinese' },
+        //             { text: 'Japanese', link: '/language/japanese' }
+        //         ]
+        //     }
+        // ],
+        // sidebar: [
+        //     {
+        //         title: 'Group 1',
+        //         collapsable: false,
+        //         children: [
+        //             '/'
+        //         ]
+        //     },
+        //     {
+        //         title: 'Group 2',
+        //         children: ['/']
+        //     }
+        // ]
+    },
+};
